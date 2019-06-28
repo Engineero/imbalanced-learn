168aa2b7786eea52856ed6d0d136a0479c3c6749
Engineero <Engineero@users.noreply.github.com>
2019-06-28 11:18:23 -0600

    Randomly cutting things to make autoencoders work

--- imblearn/under_sampling/_prototype_selection/_random_under_sampler.py
+++ imblearn/under_sampling/_prototype_selection/_random_under_sampler.py
@@ -97,7 +97,7 @@ RandomUnderSampler # doctest: +NORMALIZE_WHITESPACE
 
     @staticmethod
     def _check_X_y(X, y):
-        y, binarize_y = check_target_type(y, indicate_one_vs_all=True)
+        # y, binarize_y = check_target_type(y, indicate_one_vs_all=True)
         X = check_array(X, accept_sparse=['csr', 'csc'], dtype=None,
                         allow_nd=True)
         y = check_array(y, accept_sparse=['csr', 'csc'], dtype=None,
